f870a72d2c5f61f09ff928d9b0ccc161f9462eb8
Emdadul Haque <Emdad-Pc@DESKTOP-0SM6RSD>
2023-03-22 11:59:46 +0600

    Add project files.

---
+++ ALL Dot Net Helper/Controllers/ThirdpartyApiCallController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ALL_Dot_Net_Helper.Controllers
+{
+    [Route("helper/[controller]")]
+    [ApiController]
+    public class ThirdpartyApiCallController : ControllerBase
+    {
+        public ThirdpartyApiCallController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> CreateGetAPICall(string path)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string product = string.Empty;
+                HttpResponseMessage response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsStringAsync();//<string>();
+                }
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Request Failed");
+            }
+        }
+
+    }
+}
